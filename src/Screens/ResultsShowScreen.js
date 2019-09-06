6f4ee3c28bb10431973852c8fd18d685bef643f7
unknown <roselin.redlaboral@gmail.com>
2019-09-06 16:00:58 -0500

    Se agregaron las imagenes a  ResultsShowScreen

--- src/Screens/ResultsShowScreen.js
+++ src/Screens/ResultsShowScreen.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import {Text, View, Image, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View, Image, StyleSheet, FlatList} from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsScreenShow = ({navigation}) => {
+    const [result, setResult] = useState(null);
     const id = navigation.getParam('id');
-    console.log(id);
+    
+    const getResult = async id => {
+       const response = await yelp.get(`/${id}`);
+       setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
+    if (!result) {
+        return null;
+    }
+
     return (
         <View>
-            <Text>Results Show</Text>
+            <Text>{result.name}</Text>
+            <FlatList
+                data = {result.photos}
+                keyExtractor = {(photo) => photo}
+                renderItem = {({item}) => {
+                    return <Image style= {styles.image} source={{uri: item}}/>
+                }}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        height: 200,
+        width: 300
+    }
+});
 
 export default ResultsScreenShow;
